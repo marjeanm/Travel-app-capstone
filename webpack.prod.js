1efddd51dce6a16fd97508394235c5292489d887
Marjean D Mayo-baker <marjean_m@hotmail.com>
2021-03-12 01:49:11 -0500

    created file module exports and plugins prod

---
+++ webpack.prod.js
@@ -0,0 +1,41 @@
+const path = require('path')
+const webpack = require('webpack')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const OptimizeCSSAssetsPlugin = require('optimize-css-assets-webpack-plugin');
+
+
+module.exports = {
+    mode:'production',
+    entry: '/src/client/index.js',
+    output: {
+        libraryTarget: 'var',
+        library: 'Client'
+    },
+    optimization: {
+        minimizer: [new OptimizeCSSAssetsPlugin({})],
+        },
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                loader: "babel-loader"
+            },
+            {
+                test: /\.scss$/,
+                use: [MiniCssExtractPlugin.loader, "css-loader", "sass-loader"],
+                },
+
+
+        ]
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: "./src/Client/views/index.html",
+            filename: "./index.html",
+        }),
+        new MiniCssExtractPlugin({ filename: "[name].css" })
+    ]
+}
+
