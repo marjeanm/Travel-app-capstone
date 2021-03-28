5fe633c86b7c99805554857516f638d6ed42903c
Marjean D Mayo-baker <marjean_m@hotmail.com>
2021-03-28 13:12:51 -0400

    added index.html for project

--- src/Client/index.js
+++ src/Client/index.js
@@ -1,8 +1,11 @@
 import './styles/style.scss'
+import'./styles/responsive.scss'
 import{ performAction} from './js/app'
+import{handleSubmit} from './js/handleSubmit'
 /*our index.js file inside the client folder should import the main function of your application javascript, it should import your scss, and it should export your main function from your application javascript.*/
-alert("Marjean");
 
-export {
-    performAction
+
+export{
+    performAction,
+    handleSubmit
 }
